84aac7d418e573fb009206da6bb2d48fba2ee7ee
XXName404 <135708821+XXName404@users.noreply.github.com>
2026-03-03 00:00:48 +0100

    Update index.js

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ const makeCommits = (n) => {
   const x = random.int(0, 54);
   const y = random.int(0, 5);
   const date = moment()
-    .subtract(getRndInteger(0,7), "y")
+    .subtract(getRndInteger(0,15), "y")
     .add(1, "d")
     .add(x, "w")
     .add(y, "d")
@@ -49,5 +49,5 @@ const makeCommits = (n) => {
   });
 };
 
-makeCommits(2125);
+makeCommits(25);
 
